c872a22a9f612bdb554758b006c18cb3f0b96045
cab-mikee <mike.cabz32@gmail.com>
2025-01-06 19:43:51 +0800

    docs: improve sponsor docs

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -3,7 +3,7 @@ import { transformerTwoslash } from '@shikijs/vitepress-twoslash'
 import { withPwa } from '@vite-pwa/vitepress'
 import { defineConfig } from 'vitepress'
 
-import viteConfig from './vite.config'
+import vite from './vite.config'
 
 // https://vitepress.dev/reference/site-config
 
@@ -151,6 +151,6 @@ export default withPwa(
       ],
     },
 
-    vite: viteConfig,
+    vite,
   }),
 )
