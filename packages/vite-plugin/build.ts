c15a18276ee9067b748588cad5c5099d35f243b7
Chris <chrisbreuer93@gmail.com>
2025-05-11 18:57:01 -0700

    chore: housekeeping

--- packages/vite-plugin/build.ts
+++ packages/vite-plugin/build.ts
@@ -1,15 +1,13 @@
 import { dts } from 'bun-plugin-dtsx'
 
-console.log('Building bun-plugin...')
+console.log('Building vite-plugin...')
 
 await Bun.build({
   entrypoints: ['./src/index.ts'],
   outdir: './dist',
   format: 'esm',
   target: 'node',
-  minify: true,
-  splitting: true,
   plugins: [dts()],
 })
 
-console.log('Built bun-plugin')
+console.log('Built vite-plugin')
